792ed43c68ca7123b630fcb5811b4da2113ea999
Charlie83 <carlosgomezsoza@gmail.com>
2018-10-05 04:09:07 -0600

    part of product

---
+++ WebApp/carrito_virtual/src/componentes/product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Login extends React.Component {
+	constructor(props) {
+    
+	super(props);
+    
+	this.state = {
+      name: "",
+	  price: "",
+	  amount: "",
+    };
+	
+	
+	
+	render(){
+		<div>
+			<h1></h1>
+			<p></p>
+			<p></p>
+		</div>
+	}
+	
+	
+	}
+	
+	const style = {
+		product_body : {
+			text-align: center;
+			margin: 0 auto;
+		}
+		
+		
+	}
